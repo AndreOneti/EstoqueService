307e0532d459b60942bcbc72c6e5b5adb56494fa
Andre Luiz <andreluiz.onete@hotmail.com>
2019-07-11 23:31:04 -0300

    Finalização dos metodos do Serviço

--- ServicoEstoqueLibrary/Service1.svc.cs
+++ ServicoEstoqueLibrary/Service1.svc.cs
@@ -13,6 +13,9 @@ namespace ServicoEstoqueLibrary
 {
     // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Service1" no arquivo de código, svc e configuração ao mesmo tempo.
     // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Service1.svc ou Service1.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    // WCF service that implements the service contract
+    // This implementation performs minimal error checking and exception handling
+    [AspNetCompatibilityRequirements( RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class Service1 : IService1, IService2
     {
         public bool AdicionarEstoque(string NumeroProduto, int Quantidade)
@@ -68,12 +71,47 @@ namespace ServicoEstoqueLibrary
 
         public bool IncluirProduto(Produto Produto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Conect ao banco de dados
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    database.ProdutoEstoque.Add(new ProdutoEstoque() {
+                        NumeroProduto = Produto.NumeroProduto,
+                        NomeProduto = Produto.NomeProduto,
+                        DescricaoProduto = Produto.DescricaoProduto,
+                        EstoqueProduto = Produto.EstoqueProduto
+                    });
+                    database.SaveChanges();
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
         }
 
         public List<string> ListarProdutos()
         {
-            throw new NotImplementedException();
+            List<string> produtoEstoque = new List<string>();
+            try
+            {
+                // Conectar ao banco
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    // Busca  id para estoque especifico
+                    produtoEstoque = (from p in database.ProdutoEstoque select p.NomeProduto).ToList();
+
+                }
+            }
+            catch
+            {
+                return produtoEstoque;
+            }
+
+            return produtoEstoque;
         }
 
         public bool RemoverEstoque(string NumeroProduto, int Quantidade)
@@ -134,7 +172,33 @@ namespace ServicoEstoqueLibrary
 
         public Produto VerProduto(string NumeroProduto)
         {
-            throw new NotImplementedException();
+            Produto produto = new Produto();
+            try
+            {
+                // Conectar ao banco
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    // Busca  id para estoque especifico
+                    ProdutoEstoque produtoEstoque = (from p in database.ProdutoEstoque
+                                                     where String.Compare(p.NumeroProduto, NumeroProduto) == 0
+                                                     select p).First();
+
+                    Produto product = new Produto()
+                    {
+                        NumeroProduto = produtoEstoque.NumeroProduto,
+                        NomeProduto = produtoEstoque.NomeProduto,
+                        DescricaoProduto = produtoEstoque.DescricaoProduto,
+                        EstoqueProduto = produtoEstoque.EstoqueProduto
+                    };
+                    produto = product;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+            return produto;
         }
     }
 }
